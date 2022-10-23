a3043aa27d51928627ee4ccdddfdf66cb6948bc9
shbaek1997 <shbaek1997@gmail.com>
2022-10-24 01:05:57 +0900

    Feat:added ApexChart and candlestick chart

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1>Test First Stock Chart</h1>
     <App />
   </React.StrictMode>
 );
