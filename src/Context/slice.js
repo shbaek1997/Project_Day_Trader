de5117349a156923b1a2631dd6b4e78b502be2d1
shbaek1997 <shbaek1997@gmail.com>
2022-11-10 00:13:23 +0900

    Fix: initial state of auth slice to set user in local storage

--- src/Context/slice.js
+++ src/Context/slice.js
@@ -1,12 +1,38 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import authService from '../Utils/auth-service';
 
-const findUser = async () => {
-  const user = await authService.checkUser();
-  return user;
-};
-const user = findUser();
+const userString = window.localStorage.getItem('user');
+const user = JSON.parse(userString);
 const initialState = user ? { isLoggedIn: true, ...user } : { isLoggedIn: false, user: null };
+
+export const signup = createAsyncThunk(
+  'auth/signup',
+  async ({ email, nickname, password, repeatPassword }, thunkAPI) => {
+    try {
+      const response = await authService.signup(email, nickname, password, repeatPassword);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const login = createAsyncThunk('auth/login', async ({ email, password }, thunkAPI) => {
+  try {
+    const user = await authService.login(email, password);
+    const userString = JSON.stringify(user);
+    window.localStorage.setItem('user', userString);
+    return { user };
+  } catch (error) {
+    console.log(error);
+    return thunkAPI.rejectWithValue(error.response);
+  }
+});
+
+export const logout = createAsyncThunk('auth/logout', async () => {
+  await authService.logout();
+});
 const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -31,34 +57,5 @@ const authSlice = createSlice({
     }
   }
 });
-
-export const signup = createAsyncThunk(
-  'auth/signup',
-  async ({ email, nickname, password, repeatPassword }, thunkAPI) => {
-    try {
-      const response = await authService.signup(email, nickname, password, repeatPassword);
-      return response.data;
-    } catch (error) {
-      console.log(error);
-      return thunkAPI.rejectWithValue(error.response.data);
-    }
-  }
-);
-
-export const login = createAsyncThunk('auth/login', async ({ email, password }, thunkAPI) => {
-  try {
-    const response = await authService.login(email, password);
-    const { data } = response;
-    return { user: data };
-  } catch (error) {
-    console.log(error);
-    return thunkAPI.rejectWithValue(error.response.data);
-  }
-});
-
-export const logout = createAsyncThunk('auth/logout', async () => {
-  await authService.logout();
-});
-
 const { reducer } = authSlice;
 export default reducer;
